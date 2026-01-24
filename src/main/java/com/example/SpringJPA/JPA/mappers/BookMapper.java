e45e7d8a97474e3aa7be70ec9df3f25e63c4ea2c
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2026-01-24 19:18:02 +0530

    Added @Mapping annotations to BookMapper to bridge 'authorDto' and 'authorEntity'.

--- src/main/java/com/example/SpringJPA/JPA/mappers/BookMapper.java
+++ src/main/java/com/example/SpringJPA/JPA/mappers/BookMapper.java
@@ -3,10 +3,14 @@ package com.example.SpringJPA.JPA.mappers;
 import com.example.SpringJPA.JPA.domain.dto.BookDto;
 import com.example.SpringJPA.JPA.domain.entities.BookEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel ="Spring")
 public interface BookMapper {
 //    to and from methods
+
+    @Mapping(source="authorDto",target = "authorEntity")
     BookEntity  mapFrom(BookDto bookDto);
+    @Mapping(source="authorEntity",target = "authorDto")
     BookDto mapTo(BookEntity bookEntity);
 }
